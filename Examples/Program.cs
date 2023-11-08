1f1abc1ab01ad473b37f722438a1a7b24c6c2a6d
Arthur Caldeira <arthur.caldeira@outlook.com>
2023-11-08 12:00:37 -0300

    Add project files.

---
+++ Examples/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StringExtension;
+
+Console.WriteLine("Informe o texto que deseja dividir e prescione ENTER:");
+var textToSplit = Console.ReadLine();
+
+Console.WriteLine("Qual o tamanho máximo de cada parte?");
+var splitSize = Console.ReadLine();
+
+var splittedText = textToSplit.SplitBy(Convert.ToInt32(splitSize));
+
+Console.WriteLine("Texto depois de dividido:");
+Console.WriteLine(string.Join(Environment.NewLine, splittedText));
+
+Console.ReadKey();
---
+++ StringExtension/StringExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace StringExtension
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Split string by length
+        /// </summary>
+        /// <param name="text">String to be splitted</param>
+        /// <param name="chunkLength">Length size</param>
+        /// <returns>Enumerable with splitted stirng</returns>
+        public static IEnumerable<string> SplitBy(this string text, int chunkLength)
+        {
+            if (string.IsNullOrEmpty(text)) throw new ArgumentException();
+
+            if (chunkLength < 1) throw new ArgumentException();
+
+            for (int i = 0; i < text.Length; i += chunkLength)
+            {
+                if (chunkLength + i > text.Length)
+                    chunkLength = text.Length - i;
+
+                yield return text.Substring(i, chunkLength);
+            }
+        }
+    }
+}
